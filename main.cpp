eba9013531e614dd14b4c70430141064f0a7e8f9
Raynia <46100945+Raynia@users.noreply.github.com>
2021-09-01 23:35:31 +0900

    타이틀 화면 클릭 이벤트 추가 외
    - 타이틀 화면 클릭 이벤트 추가
    - 리소스 폴더 세분화
    - 영문 폰트 변경
    (Arial -> Dynastium-kjvy(Public domain))
    - 사용하지 않는 테스트 리소스 제거

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 #include <ctime>
 #include <vector>
 
+#define GRAPHIC_RESOURCE "resource/graphics/"
+#define MUSIC_RESOURCE "resource/music/"
+#define FONTS_RESOURCE "resource/fonts/"
+
 int main()
 {
 	///////////////////////////////////////////
@@ -30,10 +34,10 @@ int main()
 
 	const int32_t windowFullScreen = sf::Style::Close | sf::Style::Fullscreen;
 	const int32_t windowWindowScreen = sf::Style::Close;
-	
+		
 	sf::Font fontEnglish;
 	sf::Font fontEastAsia;
-	if (!fontEnglish.loadFromFile("c:/windows/fonts/arial.ttf") ||
+	if (!fontEnglish.loadFromFile(FONTS_RESOURCE"Dynastium-Kjvy.ttf") ||
 		!fontEastAsia.loadFromFile("c:/windows/fonts/gulim.ttc"))
 		return EXIT_FAILURE;
 
@@ -42,6 +46,7 @@ int main()
 	// 어플리케이션 윈도우 최초 생성
 	//
 	///////////////////////////////////////////
+
 	sf::RenderWindow window;
 	window.create(sf::VideoMode(windowWidth, windowHeight), windowTitle, windowWindowScreen, windowContextAttr);
 
@@ -54,33 +59,57 @@ int main()
 	bool isFullscreen = false; // 전체화면 여부를 확인
 	bool isWindowCreate = true; // 윈도우 (재)생성 여부를 확인
 	bool isSceneSwitch = true; // Scene 변경 여부를 확인
-
+	bool isLoading = true;
 
 	///////////////////////////////////////////
 	//
-	// 게임 내에서 사용되는 오브젝트 변수 및 함수
+	// 오브젝트 상수
+	//
+	///////////////////////////////////////////
+
+	const sf::Color BLACK(0U, 0U, 0U);
+	const sf::Color WHITE(255U, 255U, 255U);
+	const sf::Color RED(255U, 0U, 0U);
+
+	///////////////////////////////////////////
+	//
+	// 오브젝트 변수
 	//
 	///////////////////////////////////////////
 
 	/* 그래픽 오브젝트 (Texture -> Sprite - > Font) */
 
-	sf::Texture texture1;
+	sf::Texture textureTitleBG; //타이틀 화면 배경 텍스쳐
 	sf::Texture texture2;
 
-	if (!texture1.loadFromFile("resource/background_test.jpg") ||
-		!texture2.loadFromFile("resource/logo_1.jpg"))
+	if (!textureTitleBG.loadFromFile(GRAPHIC_RESOURCE"background_test.jpg") ||
+		!texture2.loadFromFile(GRAPHIC_RESOURCE"logo_1.jpg"))
 		return EXIT_FAILURE;
 
-	texture1.setSmooth(true);
+	textureTitleBG.setSmooth(true);
 	texture2.setSmooth(true);
 
-	sf::Sprite sprite1;
+	sf::Sprite spriteTitleBG; //타이틀 화면 배경 스프라이트
 	sf::Sprite sprite2;
 
-	sprite1.setTexture(texture1);
+	spriteTitleBG.setTexture(textureTitleBG);
 	sprite2.setTexture(texture2);
 
-	/* 
+	sf::Text buttonTitleNewGame(L"New Game", fontEnglish, 50U);
+	sf::Text buttonTitleContinue(L"Continue", fontEnglish);
+	sf::Text buttonTitleOptions(L"Options", fontEnglish);
+	sf::Text buttonTitleExit(L"Exit", fontEnglish);
+
+	buttonTitleNewGame.setFillColor(BLACK);
+	buttonTitleContinue.setFillColor(BLACK);
+	buttonTitleOptions.setFillColor(BLACK);
+	buttonTitleExit.setFillColor(BLACK);
+
+	float titleButtonDistance = 100.f;
+	unsigned int titleButtonY = 0U;
+	const unsigned int titleButtonCharacterSize = 45U;
+
+	/*
 	사운드 오브젝트 
 	- 배경음 등 파일 크기가 큰 오브젝트는 필요한 Scene에서 sf::Music으로 로딩
 	- 파일 크기가 작은 오브젝트는 sf::SoundBuffer에 저장 후 sf::Sound에 로딩	
@@ -91,16 +120,16 @@ int main()
 	sf::Music backgroundmusic;
 	sf::Sound soundeffect;
 
-	if (!soundeffectClick.loadFromFile("resource/click.wav") ||
-		!soundeffectDecide.loadFromFile("resource/decide.wav"))
+	if (!soundeffectClick.loadFromFile(MUSIC_RESOURCE"click.wav") ||
+		!soundeffectDecide.loadFromFile(MUSIC_RESOURCE"confirm.wav"))
 		return EXIT_FAILURE;
 
 	backgroundmusic.setVolume(50.f);
 	soundeffect.setVolume(50.f);
 
-	sf::Vector2u titleUISize(100u, 40u);
+	/*sf::Vector2u titleUISize(100u, 40u);
 	sf::Vector2u titleUIPosition(910u, 500u);
-	sf::Rect<unsigned int> rect(titleUIPosition, titleUISize);
+	sf::Rect<unsigned int> rect(titleUIPosition, titleUISize);*/
 	
 	///////////////////////////////////////////
 	//
@@ -112,10 +141,9 @@ int main()
 
 	///////////////////////////////////////////
 	//
-	// 기능 테스트를 위한 변수 및 함수
+	// 
+	// 기능 테스트 혹은 개발용 변수 및 함수
 	//
-	// - 해당 변수가 사용된 모든 변수와 함수는 기능 테스트 목적을 위함
-	// - 
 	// 
 	///////////////////////////////////////////
 
@@ -132,28 +160,13 @@ int main()
 	sf::Text testTextOptions, testTextMenuButton;
 	testTextOptions.setFont(fontEnglish);
 	testTextOptions.setString(L"Insted of Options");
-
-	testTextMenuButton.setFont(fontEnglish);
-	testTextMenuButton.setString(L"Button");	
 	
-	sf::Texture testTextureStart, testTextureOptions, testTextureExtra, testTextureExit;
-	if (!testTextureStart.loadFromFile("resource/testStartImage.png") ||
-		!testTextureOptions.loadFromFile("resource/testOptionsImage.png") ||
-		!testTextureExtra.loadFromFile("resource/testExtraImage.png") ||
-		!testTextureExit.loadFromFile("resource/testExitImage.png"))
-		return EXIT_FAILURE;
-
-	sf::Sprite testSpriteStart, testSpriteOptions, testSpriteExtra, testSpriteExit;
-	testSpriteStart.setTexture(testTextureStart);
-	testSpriteOptions.setTexture(testTextureOptions);
-	testSpriteExtra.setTexture(testTextureExtra);
-	testSpriteExit.setTexture(testTextureExit);
-
 	sf::Sprite testSpriteforView;
 	testSpriteforView.setTexture(texture2);
 
+	sf::Text debugAlphaVersionInfo(L"This game is now currently development", fontEnglish);
+	debugAlphaVersionInfo.setFillColor(sf::Color(255U, 0U, 0U));
 
-	float testMenuButtonBetweenDistance = 100.f;
 	float testMenuButtonPositionY = 0.f;	
 
 	sf::Rect<float> testRect;
@@ -164,14 +177,19 @@ int main()
 	while (window.isOpen())
 	{
 		sf::Event event;
-
+		
 		while (window.pollEvent(event))
 		{
-			switch(event.type)
+			switch (event.type)
 			{
+
+			//창 모드에서 닫기 버튼 클릭 시 이벤트
+
 			case sf::Event::Closed:
 				window.close();
 				break;
+
+			//키보드 입력 시 이벤트
 
 			case sf::Event::KeyPressed:
 
@@ -184,7 +202,7 @@ int main()
 					window.create(sf::VideoMode::getDesktopMode(), windowTitle, windowFullScreen, windowContextAttr);
 					isFullscreen = true;
 					isWindowCreate = true;
-				}				
+				}
 				else if (event.key.code == sf::Keyboard::S && isFullscreen)
 				{
 					window.create(sf::VideoMode(windowWidth, windowHeight), windowTitle, windowWindowScreen, windowContextAttr);
@@ -194,34 +212,74 @@ int main()
 
 				if (event.key.code == sf::Keyboard::D)
 				{
-					if (testScene1 && !testScene2)
-					{
-						testScene1 = false;
-						testScene2 = true;
-					}
-					else/*if (!scene1 && scene2)*/
+					if (!testScene1 && testScene2)
 					{
 						testScene1 = true;
 						testScene2 = false;
+						isSceneSwitch = true;
 					}
-					isSceneSwitch = true;
 				}
 
-				if (event.key.code == sf::Keyboard::F)
+				if (event.key.code == sf::Keyboard::Escape)
 				{
-					if (testTitleOptionSwitch)
-						testTitleOptionSwitch = false;
-					else
-						testTitleOptionSwitch = true;
+					window.close();
 				}
+
 				break;
+			
+			// 마우스 클릭 시 이벤트
+			// Button 클래스와 같이 모듈화를 통해 코드 간략화 필요
 
 			case sf::Event::MouseButtonPressed:
-				break;
+				if (testScene1)
+				{
+					if (event.mouseButton.x >= buttonTitleNewGame.getGlobalBounds().left &&
+						event.mouseButton.x <= buttonTitleNewGame.getGlobalBounds().left + buttonTitleNewGame.getGlobalBounds().width &&
+						event.mouseButton.y >= buttonTitleNewGame.getGlobalBounds().top &&
+						event.mouseButton.y <= buttonTitleNewGame.getGlobalBounds().top + buttonTitleNewGame.getGlobalBounds().height)
+					{						
+						testScene1 = false;
+						testScene2 = true;
+						
+						isSceneSwitch = true;
+					}
 
+					if (event.mouseButton.x >= buttonTitleContinue.getGlobalBounds().left &&
+						event.mouseButton.x <= buttonTitleContinue.getGlobalBounds().left + buttonTitleContinue.getGlobalBounds().width &&
+						event.mouseButton.y >= buttonTitleContinue.getGlobalBounds().top &&
+						event.mouseButton.y <= buttonTitleContinue.getGlobalBounds().top + buttonTitleContinue.getGlobalBounds().height)
+					{
+
+						std::cout << "Continue button pressed\n";
+						soundeffect.setBuffer(soundeffectDecide);
+						soundeffect.play();
+					}
+
+					if (event.mouseButton.x >= buttonTitleOptions.getGlobalBounds().left &&
+						event.mouseButton.x <= buttonTitleOptions.getGlobalBounds().left + buttonTitleOptions.getGlobalBounds().width &&
+						event.mouseButton.y >= buttonTitleOptions.getGlobalBounds().top &&
+						event.mouseButton.y <= buttonTitleOptions.getGlobalBounds().top + buttonTitleOptions.getGlobalBounds().height)
+					{
+						std::cout << "Options button pressed\n";
+						soundeffect.setBuffer(soundeffectDecide);
+						soundeffect.play();
+					}
+
+					if (event.mouseButton.x >= buttonTitleExit.getGlobalBounds().left &&
+						event.mouseButton.x <= buttonTitleExit.getGlobalBounds().left + buttonTitleExit.getGlobalBounds().width &&
+						event.mouseButton.y >= buttonTitleExit.getGlobalBounds().top &&
+						event.mouseButton.y <= buttonTitleExit.getGlobalBounds().top + buttonTitleExit.getGlobalBounds().height)
+						window.close();
+					
+				}
+				break;
+			
 			default:
 				break;
-			}			
+			}
+				
+			
+			
 		}
 		
 		// 윈도우 (재)생성 시 실행되는 기능
@@ -231,40 +289,42 @@ int main()
 
 		if (isWindowCreate)
 		{
-			sprite1.setScale(sf::Vector2f(window.getSize().x / sprite1.getLocalBounds().width,
-				window.getSize().y / sprite1.getLocalBounds().height));
-			sprite2.setScale(sf::Vector2f(window.getSize().x / sprite1.getLocalBounds().width,
-				window.getSize().y / sprite1.getLocalBounds().height));
-			testSpriteforView.setScale(sf::Vector2f(window.getSize().x / sprite1.getLocalBounds().width,
-				window.getSize().y / sprite1.getLocalBounds().height));
+			//Sprite 정보 변경
 
-			std::cout << sprite2.getLocalBounds().width << "&" << sprite2.getLocalBounds().height << "\n";
-			std::cout << sprite2.getGlobalBounds().width << "&" << sprite2.getGlobalBounds().height << "\n";
-			testSpriteforView.setPosition(sf::Vector2f(sprite2.getGlobalBounds().width, 0));
+			spriteTitleBG.setScale(sf::Vector2f(window.getSize().x / spriteTitleBG.getLocalBounds().width,
+				window.getSize().y / spriteTitleBG.getLocalBounds().height));
+			sprite2.setScale(sf::Vector2f(window.getSize().x / sprite2.getLocalBounds().width,
+				window.getSize().y / spriteTitleBG.getLocalBounds().height));
+						
+			//Text 정보 변경
 
-			/*testSpriteStart.setScale(sf::Vector2f(window.getSize().x / testSpriteStart.getGlobalBounds().width,
-				window.getSize().y / sprite1.getLocalBounds().height));*/
-			/*testSpriteOptions.setScale(sf::Vector2f(window.getSize().x / testSpriteOptions.getLocalBounds().width,
-				window.getSize().y / sprite1.getLocalBounds().height));*/
+			titleButtonDistance = 120.f * (window.getSize().x / 1920.f);
+			titleButtonY = static_cast<unsigned int> (window.getSize().y * (3.f / 4.f));
 
-			testMenuButtonPositionY = (window.getSize().y / 4 * 3) - (testSpriteStart.getLocalBounds().height / 2);
-			testMenuButtonBetweenDistance = testSpriteStart.getLocalBounds().width / 2;
+			debugAlphaVersionInfo.setCharacterSize(static_cast<unsigned int>(30 * (window.getSize().x / 1920.f)));
 
-			testSpriteStart.setPosition(
-				((window.getSize().x / 2) - testSpriteStart.getLocalBounds().width * 1.5) - testMenuButtonBetweenDistance,
-				testMenuButtonPositionY);
+			buttonTitleNewGame.setCharacterSize(titleButtonCharacterSize * (window.getSize().x / 1920.f));
+			buttonTitleContinue.setCharacterSize(titleButtonCharacterSize * (window.getSize().x / 1920.f));
+			buttonTitleOptions.setCharacterSize(titleButtonCharacterSize * (window.getSize().x / 1920.f));
+			buttonTitleExit.setCharacterSize(titleButtonCharacterSize * (window.getSize().x / 1920.f));
 
-			testSpriteOptions.setPosition(
-				testSpriteStart.getPosition().x + testSpriteStart.getLocalBounds().width + testMenuButtonBetweenDistance,
-				testMenuButtonPositionY);
+			
+			buttonTitleContinue.setPosition(
+				window.getSize().x / 2 - buttonTitleContinue.getGlobalBounds().width,
+				titleButtonY);
+			buttonTitleNewGame.setPosition(
+				buttonTitleContinue.getGlobalBounds().left - (titleButtonDistance + buttonTitleNewGame.getGlobalBounds().width),
+				titleButtonY);
+			buttonTitleOptions.setPosition(
+				buttonTitleContinue.getGlobalBounds().width + buttonTitleContinue.getGlobalBounds().left + titleButtonDistance,
+				titleButtonY);
+			buttonTitleExit.setPosition(
+				buttonTitleOptions.getGlobalBounds().width + buttonTitleOptions.getGlobalBounds().left + titleButtonDistance,
+				titleButtonY);
 
-			testSpriteExit.setPosition(
-				testSpriteOptions.getPosition().x + testSpriteStart.getLocalBounds().width + testMenuButtonBetweenDistance,
-				testMenuButtonPositionY);
+			//View 사이즈 변경
 
-			testRect = testSpriteStart.getLocalBounds();
-
-			sf::FloatRect visibleArea(window.getSize().x / 2, 0.f, window.getSize().x, window.getSize().y);
+			sf::FloatRect visibleArea(0.f, 0.f, window.getSize().x, window.getSize().y);
 			window.setView(sf::View(visibleArea));
 
 			isWindowCreate = false;
@@ -277,21 +337,23 @@ int main()
 		//오브젝트가 윈도우에 그려지는 구간
 
 		window.clear();
-		
+
 		if (testScene1)
 		{			
-			window.draw(sprite1);
-			window.draw(testSpriteStart);
-			window.draw(testSpriteOptions);
-			window.draw(testSpriteExit);
-			window.draw(testSpriteforView);
+			window.draw(spriteTitleBG);
+
 			if (isSceneSwitch)
 			{
 				backgroundmusic.stop();
-				backgroundmusic.openFromFile("resource/title.ogg");
+				backgroundmusic.openFromFile(MUSIC_RESOURCE"title.ogg");
 				backgroundmusic.play();
 				isSceneSwitch = false;
 			}
+			
+			window.draw(buttonTitleNewGame);
+			window.draw(buttonTitleContinue);
+			window.draw(buttonTitleOptions);
+			window.draw(buttonTitleExit);
 
 			if (testTitleOptionSwitch)
 				window.draw(testTextOptions);
@@ -304,12 +366,13 @@ int main()
 			if (isSceneSwitch)
 			{
 				backgroundmusic.stop();
-				backgroundmusic.openFromFile("resource/test.ogg");
+				backgroundmusic.openFromFile(MUSIC_RESOURCE"test.ogg");
 				backgroundmusic.play();
 				isSceneSwitch = false;
 			}
 		}
 
+		window.draw(debugAlphaVersionInfo);
 		window.display();
 	}
 
